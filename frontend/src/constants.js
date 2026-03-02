76d5eb6f00f56504db85b5f31584ac53ff9ef0b3
sudheer <sudheermishra8587@gmail.com>
2026-03-02 22:16:12 +0530

    add constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "https://rfbqprvpxdyfdwhmuqvk.supabase.co/";
+export const SIGNUP_URL = BASE_URL + "auth/v1/signup";
+export const LOGOUT_URL = BASE_URL + "auth/v1/logout";
+export const LOGIN_URL = BASE_URL + "auth/v1/token?grant_type=password";
+export const SUPABASE_API_KEY =
+  "sb_publishable_OnpEuyjuZALHa_Cwvg1d5Q_g5jbQvhK";
+export const GET_ALL_COURSES = BASE_URL + "rest/v1/courses?select=*";
+export const STRIPE_PUBLISHABLE_KEY =
+  "pk_test_51T6PH5GkacHtVf4oqe2finBdXqK90dB0HCSW4TYqGFlkBBub23TICVfif4zh3ZdScAvS8ttXcgV6KFOGEA338dc800Ofjq9FuT";
